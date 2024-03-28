95f36a04c3ccabcd6f9662c7134b7c5a769619f3
CavneyMP01 <matthewcavney@yahoo.co.uk>
2024-03-28 02:57:43 +0000

    Place holder text updated, front page close to inital specifcation

--- resources/views/components/site-area-triple-block.blade.php
+++ resources/views/components/site-area-triple-block.blade.php
@@ -5,24 +5,24 @@
             <div class="w-full md:w-1/3 px-4 mb-4 md:mb-0">
                 <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg flex justify-center items-center" style="height: 20vh;">
                     <div class="text-center">
-                        
-                        <img class="mx-auto h-12" 
-                        src="{{ asset('images/workout.png') }}" 
-                        alt="Workout Image">
-
-                        <h3 class="text-sm mt-2 text-gray-900 dark:text-gray-100">{{ __('Workout Plan') }}</h3>
-                    </div>
-                </div>
-            </div>
-            <div class="w-full md:w-1/3 px-4 mb-4 md:mb-0">
-                <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg flex justify-center items-center" style="height: 20vh;">
-                    <div class="text-center">
 
                         <img class="mx-auto h-12"
                         src="{{ asset('images/mealplan.png') }}"
                         alt="Meal Plan Image">
 
                         <h3 class="text-sm mt-2 text-gray-900 dark:text-gray-100">{{ __('Meal Plan') }}</h3>
+                    </div>
+                </div>
+            </div>
+            <div class="w-full md:w-1/3 px-4 mb-4 md:mb-0">
+                <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg flex justify-center items-center" style="height: 20vh;">
+                    <div class="text-center">
+                        
+                        <img class="mx-auto h-12" 
+                        src="{{ asset('images/workout.png') }}" 
+                        alt="Workout Image">
+
+                        <h3 class="text-sm mt-2 text-gray-900 dark:text-gray-100">{{ __('Workout Plan') }}</h3>
                     </div>
                 </div>
             </div>
--- resources/views/components/stats-grid.blade.php
+++ resources/views/components/stats-grid.blade.php
@@ -2,28 +2,30 @@
   <div class="mx-auto max-w-7xl">
     <div class="grid grid-cols-1 gap-px bg-white/5 sm:grid-cols-2 lg:grid-cols-4">
       <div class="bg-gray-900 px-4 py-6 sm:px-6 lg:px-8">
-        <p class="text-sm font-medium leading-6 text-gray-400">Number of deploys</p>
+        <p class="text-sm font-medium leading-6 text-gray-400">Days in a row</p>
         <p class="mt-2 flex items-baseline gap-x-2">
-          <span class="text-4xl font-semibold tracking-tight text-white">405</span>
+          <span class="text-4xl font-semibold tracking-tight text-white">9</span>
         </p>
       </div>
       <div class="bg-gray-900 px-4 py-6 sm:px-6 lg:px-8">
-        <p class="text-sm font-medium leading-6 text-gray-400">Average deploy time</p>
+        <p class="text-sm font-medium leading-6 text-gray-400">Current weight / Target weight</p>
         <p class="mt-2 flex items-baseline gap-x-2">
-          <span class="text-4xl font-semibold tracking-tight text-white">3.65</span>
-          <span class="text-sm text-gray-400">mins</span>
+          <span class="text-4xl font-semibold tracking-tight text-white">65</span>
+          <span class="text-sm text-gray-400">kg</span>
+          <span class="text-4xl font-semibold tracking-tight text-white">/ 70</span>
+          <span class="text-sm text-gray-400">kg</span>
         </p>
       </div>
       <div class="bg-gray-900 px-4 py-6 sm:px-6 lg:px-8">
-        <p class="text-sm font-medium leading-6 text-gray-400">Number of servers</p>
+        <p class="text-sm font-medium leading-6 text-gray-400">Kcal target</p>
         <p class="mt-2 flex items-baseline gap-x-2">
-          <span class="text-4xl font-semibold tracking-tight text-white">3</span>
+          <span class="text-4xl font-semibold tracking-tight text-white">2700kcal</span>
         </p>
       </div>
       <div class="bg-gray-900 px-4 py-6 sm:px-6 lg:px-8">
-        <p class="text-sm font-medium leading-6 text-gray-400">Success rate</p>
+        <p class="text-sm font-medium leading-6 text-gray-400">Total Weight Lifted</p>
         <p class="mt-2 flex items-baseline gap-x-2">
-          <span class="text-4xl font-semibold tracking-tight text-white">98.5%</span>
+          <span class="text-4xl font-semibold tracking-tight text-white">1,245kg</span>
         </p>
       </div>
     </div>
