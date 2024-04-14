d6144428ed9eb494a8579ba6e98a13b202bed37c
CavneyMP01 <matthewcavney@yahoo.co.uk>
2024-04-14 04:13:39 +0100

    show and process form fucntions added to controller class

--- app/Http/Controllers/MetricsController.php
+++ app/Http/Controllers/MetricsController.php
@@ -6,5 +6,23 @@ use Illuminate\Http\Request;
 
 class MetricsController extends Controller
 {
-    //
+
+        /**
+     * Display the metrics form page
+     */
+    public function showForm()
+    {
+        // Returns the view for the metrics form
+
+        return view('metrics.form');
+    }
+
+        /**
+     * Process the submitted metrics form.
+     */
+
+    public function processForm(Request $request)
+    {
+            //TODO        
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\MetricsController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -15,6 +16,8 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
+    Route::get('/metrics', [MetricsController::class, 'showForm'])->name('metrics.form');
+    Route::post('/metrics', [MetricsController::class, 'processForm'])->name('metrics.process');
+    });
 
 require __DIR__.'/auth.php';
