28e3a01fb9f76a5afef11d7c0d635906edf9de2a
CavneyMP01 <matthewcavney@yahoo.co.uk>
2024-04-14 18:31:17 +0100

    form view created extending header, and brought in the tailwind form that will be remade for purpose of the metric form

--- resources/views/metrics/form.blade.php
+++ resources/views/metrics/form.blade.php
@@ -1,5 +1,9 @@
-@extends('layouts.app')
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800">
+            User Metrics
+        </h2>
+    </x-slot>
 
-@section('content')
-
-@endsection
+    <x-metrics-form />
+</x-app-layout>
