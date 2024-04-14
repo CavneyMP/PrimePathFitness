090337111ecac2cf40f21c5e61b65db1a11323a5
CavneyMP01 <matthewcavney@yahoo.co.uk>
2024-04-14 03:10:54 +0100

    New route for mectric collection form

--- database/migrations/2024_04_13_010854_create_workouts_table.php
+++ database/migrations/2024_04_13_010854_create_workouts_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
     /**
-     * Pgit redefined workouts
+     * Predefined workouts.
      */
 return new class extends Migration
 {
