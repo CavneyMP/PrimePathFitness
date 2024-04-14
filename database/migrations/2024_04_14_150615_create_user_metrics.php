8f00159efc4cebe4c6b3132ed888ea765b30b7ab
CavneyMP01 <matthewcavney@yahoo.co.uk>
2024-04-14 16:31:55 +0100

    Re commit, forgot to add files

---
+++ database/migrations/2024_04_14_150615_create_user_metrics.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('user_metrics', function (Blueprint $table) {
+            $table->foreignId('user_id');
+            $table->integer('age');
+            $table->float('weight');
+            $table->float('height');
+            $table->string('activity_level');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('user_metrics');
+    }
+};
