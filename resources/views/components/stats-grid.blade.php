9c5b50fc03bca501ee317edb31c6664d7e9353dc
CavneyMP01 <matthewcavney@yahoo.co.uk>
2024-04-16 03:51:07 +0100

    Code format extension used for various classes

--- resources/views/components/stats-grid.blade.php
+++ resources/views/components/stats-grid.blade.php
@@ -1,33 +1,36 @@
 <div class="bg-gray-900">
-  <div class="mx-auto max-w-7xl">
-    <div class="grid grid-cols-1 gap-px bg-white/5 sm:grid-cols-2 lg:grid-cols-4">
-      <div class="bg-gray-900 px-4 py-6 sm:px-6 lg:px-8">
-        <p class="text-sm font-medium leading-6 text-gray-400">BMR</p>
-        <p class="mt-2 flex items-baseline gap-x-2">
-          <span class="text-4xl font-semibold tracking-tight text-white">{{ $metrics->bmr ?? 'N/a' }} kcal</span>
-        </p>
-      </div>
-      <div class="bg-gray-900 px-4 py-6 sm:px-6 lg:px-8">
-        <p class="text-sm font-medium leading-6 text-gray-400">Current weight / Target weight</p>
-        <p class="mt-2 flex items-baseline gap-x-2">
-          <span class="text-4xl font-semibold tracking-tight text-white">65</span>
-          <span class="text-sm text-gray-400">kg</span>
-          <span class="text-4xl font-semibold tracking-tight text-white">/ 70</span>
-          <span class="text-sm text-gray-400">kg</span>
-        </p>
-      </div>
-      <div class="bg-gray-900 px-4 py-6 sm:px-6 lg:px-8">
-        <p class="text-sm font-medium leading-6 text-gray-400">BMI</p>
-        <p class="mt-2 flex items-baseline gap-x-2">
-          <span class="text-4xl font-semibold tracking-tight text-white">{{ round($metrics->bmi) ?? 'N/a' }} </span>
-        </p>
-      </div>
-      <div class="bg-gray-900 px-4 py-6 sm:px-6 lg:px-8">
-        <p class="text-sm font-medium leading-6 text-gray-400">Daily Maintaince Kcald</p>
-        <p class="mt-2 flex items-baseline gap-x-2">
-          <span class="text-4xl font-semibold tracking-tight text-white">{{ $metrics->tdee ?? 'N/a' }} Kcal</span>
-        </p>
-      </div>
+    <div class="mx-auto max-w-7xl">
+        <div class="grid grid-cols-1 gap-px bg-white/5 sm:grid-cols-2 lg:grid-cols-4">
+            <div class="bg-gray-900 px-4 py-6 sm:px-6 lg:px-8">
+                <p class="text-sm font-medium leading-6 text-gray-400">BMR</p>
+                <p class="mt-2 flex items-baseline gap-x-2">
+                    <span class="text-4xl font-semibold tracking-tight text-white">{{ $metrics->bmr ?? 'N/a' }}
+                        kcal</span>
+                </p>
+            </div>
+            <div class="bg-gray-900 px-4 py-6 sm:px-6 lg:px-8">
+                <p class="text-sm font-medium leading-6 text-gray-400">Current weight / Target weight</p>
+                <p class="mt-2 flex items-baseline gap-x-2">
+                    <span class="text-4xl font-semibold tracking-tight text-white">65</span>
+                    <span class="text-sm text-gray-400">kg</span>
+                    <span class="text-4xl font-semibold tracking-tight text-white">/ 70</span>
+                    <span class="text-sm text-gray-400">kg</span>
+                </p>
+            </div>
+            <div class="bg-gray-900 px-4 py-6 sm:px-6 lg:px-8">
+                <p class="text-sm font-medium leading-6 text-gray-400">BMI</p>
+                <p class="mt-2 flex items-baseline gap-x-2">
+                    <span class="text-4xl font-semibold tracking-tight text-white">{{ round($metrics->bmi) ?? 'N/a' }}
+                    </span>
+                </p>
+            </div>
+            <div class="bg-gray-900 px-4 py-6 sm:px-6 lg:px-8">
+                <p class="text-sm font-medium leading-6 text-gray-400">Daily Maintaince Kcald</p>
+                <p class="mt-2 flex items-baseline gap-x-2">
+                    <span class="text-4xl font-semibold tracking-tight text-white">{{ $metrics->tdee ?? 'N/a' }}
+                        Kcal</span>
+                </p>
+            </div>
+        </div>
     </div>
-  </div>
 </div>
