a8b4870629f9adeb2995dcdd65594cc34b439019
CavneyMP01 <matthewcavney@yahoo.co.uk>
2024-04-16 04:58:39 +0100

    Added file

---
+++ resources/views/components/create-new-workout.blade.php
@@ -0,0 +1,21 @@
+    <!-- Outer container, max width for mobile users -->
+    <div class="mx-auto max-w-5xl px-8">
+
+            <div class="bg-gray-900 shadow sm:rounded-lg">
+                <div class="px-4 py-5 sm:p-6">
+                    <div class="sm:flex sm:items-start sm:justify-between">
+                        <div>
+                            <h3 class="text-base font-semibold leading-6 text-white">Create a New Workout plan</h3>
+                            <div class="mt-2 max-w-xl text-sm text-gray-300">
+                                <p>You can now generate custom workout plans using filters and equipment preferences!.</p>
+                            </div>
+                        </div>
+                        <div class="mt-5 sm:ml-6 sm:mt-0 sm:flex sm:flex-shrink-0 sm:items-center pt-4">
+                            <button type="button"
+                                class="inline-flex items-center rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Create
+                                plan</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+    </div> <!-- end outer container -->
