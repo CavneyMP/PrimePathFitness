dd1c1e9357120d4d64bc49ed3f92e8f98c44a7f3
CavneyMP01 <matthewcavney@yahoo.co.uk>
2024-04-16 02:05:29 +0100

    Same as before, caused syntax error in prior comit, rectified.

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\UserMetric;
-
+use Illuminate\Support\Facades\Log; // Import the Log facade
 
 class HomeController extends Controller
 {
@@ -17,21 +17,15 @@ class HomeController extends Controller
         // Fetching the latest metrics for the user from the database table
         $metrics = UserMetric :: where ('user_id', auth()->id())->latest()->first();
 
-                // DEBUGGER
-                Log::debug
-                ('found', ['metrics' => $metrics ]);
+        // DEBUGGER
+        Log::debug('found', ['metrics' => $metrics]);
 
-                // DEBUGGER
-                if (!$metrics) {
-                    Log::warning('Non found: ' . auth() - >id());
-                }
-
-
+        // DEBUGGER
+        if (!$metrics) {
+            Log::warning('Non found: ' . auth()->id());
+        }
 
         // Passing the metrics to the dashboard view
         return view ('dashboard' ,['metrics' => $metrics]);
-
-        
-
     }
 }
