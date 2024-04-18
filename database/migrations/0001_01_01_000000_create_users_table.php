2cdc3bb34ee3560dc48c785724a9c7fe3feda37b
CavneyMP01 <matthewcavney@yahoo.co.uk>
2024-04-18 20:55:41 +0100

    Moved the TDEE, BMI and BMR field to user migration file as needing to fresh database anyway.

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -18,6 +18,9 @@ return new class extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
+            $table -> float('bmi') -> nullable();
+            $table -> float('bmr') -> nullable();
+            $table -> float('tdee') -> nullable();
             $table->timestamps();
         });
 
