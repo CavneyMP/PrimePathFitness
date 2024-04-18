1e8cd1ef9c4748470357270b592e92abe5fb4327
CavneyMP01 <matthewcavney@yahoo.co.uk>
2024-04-18 23:02:33 +0100

    Adjustment to the exercises table migration

--- database/migrations/2024_04_13_000005_create_exercises_table.php
+++ database/migrations/2024_04_13_000005_create_exercises_table.php
@@ -14,11 +14,14 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('exercises', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->text('description');
-            $table->string('muscle_group');
-            $table->timestamps();
+            $table->id(); // Unique identifier for each exercise
+            $table->string('name'); // Name of the exercise
+            $table->text('description'); // Description of the exercise
+            $table->string('muscle_group'); // Muscle group targeted
+            $table->foreignId('equipment_id')->constrained('equipment'); // Specifier for the  equipment needed 
+            $table->string('exercise_type')->nullable(); // Such as strength, cardio, flexibility
+            $table->string('difficulty_level')->nullable(); // Difficulty level of the exercise
+            $table->timestamps(); // created_at and updated_at
         });
     }
 
