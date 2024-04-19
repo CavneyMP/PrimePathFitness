d4775ed40339c3b712c2ca89af6430b088166ef4
CavneyMP01 <matthewcavney@yahoo.co.uk>
2024-04-19 16:10:46 +0100

    Exercise model attributes

--- app/Models/Exercise.php
+++ app/Models/Exercise.php
@@ -8,4 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class Exercise extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['name', 'description', 'muscle_group', 'exercise_type', 'difficulty_level', 'equipment_id']; // Fields mass assignable
+
 }
