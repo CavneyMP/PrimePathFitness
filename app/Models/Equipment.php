15b9eec15b2f5951ee30de71555f8c4ca3aa919f
CavneyMP01 <matthewcavney@yahoo.co.uk>
2024-04-19 16:11:34 +0100

    Equipment  model attributes

--- app/Models/Equipment.php
+++ app/Models/Equipment.php
@@ -8,4 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class Equipment extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['name',  'description']; // Mass assingable attributes.
+
 }
