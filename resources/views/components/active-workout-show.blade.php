bc1b1ca33bc9c6045d44f72b8b705e8a66d4dbb7
CavneyMP01 <matthewcavney@yahoo.co.uk>
2024-04-20 04:39:09 +0100

    Further form design

--- resources/views/components/active-workout-show.blade.php
+++ resources/views/components/active-workout-show.blade.php
@@ -1,15 +1,15 @@
 <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 px-4 pt-4">
     <div class="mx-auto max-w-3xl">
         <div class="bg-gray-800 p-6 rounded-lg shadow-lg">
-            <h2 class="text-xl font-semibold leading-7 text-white">Customize Your Workout Plan</h2>
-            <p class="text-gray-300">Select your preferences to generate a tailored workout plan.</p>
+            <h2 class="text-xl font-semibold leading-7 text-white">$PlanName</h2>
+            <p class="text-gray-300">$PlanDescrition.</p>
 
             <ul role="list" class="divide-y divide-gray-800">
                 <li class="flex justify-between gap-x-6 py-5">
                     <div class="flex min-w-0 gap-x-4">
                         <div class="min-w-0 flex-auto">
                             <p class="text-sm font-semibold leading-6 text-white">$Exercise</p>
-                            <p class="mt-1 truncate text-xs leading-5 text-gray-400">exercise.alexander@example.com</p>
+                            <p class="mt-1 truncate text-xs leading-5 text-gray-400">$description</p>
                         </div>
                     </div>
                     <div class="hidden shrink-0 sm:flex sm:flex-col sm:items-end">
@@ -22,7 +22,7 @@
                     <div class="flex min-w-0 gap-x-4">
                         <div class="min-w-0 flex-auto">
                             <p class="text-sm font-semibold leading-6 text-white">$Exercise</p>
-                            <p class="mt-1 truncate text-xs leading-5 text-gray-400">exercise.foster@example.com</p>
+                            <p class="mt-1 truncate text-xs leading-5 text-gray-400">$description</p>
                         </div>
                     </div>
                     <div class="hidden shrink-0 sm:flex sm:flex-col sm:items-end">
@@ -35,7 +35,7 @@
                     <div class="flex min-w-0 gap-x-4">
                         <div class="min-w-0 flex-auto">
                             <p class="text-sm font-semibold leading-6 text-white">$Exercise</p>
-                            <p class="mt-1 truncate text-xs leading-5 text-gray-400">exercise.vincent@example.com</p>
+                            <p class="mt-1 truncate text-xs leading-5 text-gray-400">$description</p>
                         </div>
                     </div>
                     <div class="hidden shrink-0 sm:flex sm:flex-col sm:items-end">
@@ -52,7 +52,7 @@
                     <div class="flex min-w-0 gap-x-4">
                         <div class="min-w-0 flex-auto">
                             <p class="text-sm font-semibold leading-6 text-white">$Exercise </p>
-                            <p class="mt-1 truncate text-xs leading-5 text-gray-400">exercise.walton@example.com</p>
+                            <p class="mt-1 truncate text-xs leading-5 text-gray-400">$description/p>
                         </div>
                     </div>
                     <div class="hidden shrink-0 sm:flex sm:flex-col sm:items-end">
@@ -65,7 +65,7 @@
                     <div class="flex min-w-0 gap-x-4">
                         <div class="min-w-0 flex-auto">
                             <p class="text-sm font-semibold leading-6 text-white">$Exercise</p>
-                            <p class="mt-1 truncate text-xs leading-5 text-gray-400">exercise.henry@example.com</p>
+                            <p class="mt-1 truncate text-xs leading-5 text-gray-400">$description</p>
                         </div>
                     </div>
                     <div class="hidden shrink-0 sm:flex sm:flex-col sm:items-end">
@@ -78,7 +78,7 @@
                     <div class="flex min-w-0 gap-x-4">
                         <div class="min-w-0 flex-auto">
                             <p class="text-sm font-semibold leading-6 text-white">$Exercise</p>
-                            <p class="mt-1 truncate text-xs leading-5 text-gray-400">exercise.cook@example.com</p>
+                            <p class="mt-1 truncate text-xs leading-5 text-gray-400">$description</p>
                         </div>
                     </div>
                     <div class="hidden shrink-0 sm:flex sm:flex-col sm:items-end">
