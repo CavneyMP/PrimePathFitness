9979f4f67bc1d7144d0d86e5eb9d6accac8abe06
CavneyMP01 <matthewcavney@yahoo.co.uk>
2024-04-21 00:47:08 +0100

    Restricted data retreival to active workouts

--- app/Http/Controllers/WorkoutController.php
+++ app/Http/Controllers/WorkoutController.php
@@ -4,12 +4,17 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Workout;
+use App\Models\UserWorkout;
 
 class WorkoutController extends Controller
 {
     public function index()
     {
-        $workouts = Workout :: all(); // Will look to fetch all workouts from the DB
+        $activeWorkout = UserWorkout::where('user_id', auth()->id()) // Will look to fetch all workouts from the DB
+                            ->where('status', 'active')
+                             ->latest()
+                              ->first();
+                              
         // return workout blade view
         return view('pages.workout');
     }
