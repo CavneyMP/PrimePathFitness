b75cf97da6ce6605ba7666186f1d0adfb732bc62
CavneyMP01 <matthewcavney@yahoo.co.uk>
2024-04-21 03:04:45 +0100

    New page blade view

---
+++ resources/views/pages/mealplan-create.blade.php
@@ -0,0 +1,22 @@
+<x-app-layout>
+
+<!-- Header slot, provides page title and breif info -->
+<x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+            {{ __('Meal Plan Generator') }}
+        </h2>
+    </x-slot>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900 dark:text-gray-100">
+                    Please choose from the recipes below!
+                </div>
+            </div>
+        </div>
+    </div>
+    
+    <x-workout-builder />
+
+</x-app-layout>
