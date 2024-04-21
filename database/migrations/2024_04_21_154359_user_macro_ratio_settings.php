cc0e946a8b914700ce546e99338dedf0374e41a5
CavneyMP01 <matthewcavney@yahoo.co.uk>
2024-04-21 16:48:16 +0100

    Required feilds with defatault ratios

--- database/migrations/2024_04_21_154359_user_macro_ratio_settings.php
+++ database/migrations/2024_04_21_154359_user_macro_ratio_settings.php
@@ -6,19 +6,20 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
     public function up(): void
     {
-        //
+        Schema::create('user_nutritional_settings', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained(); // FK to users
+            $table->float('carbs_ratio') -> default(0.50); // Default 50% of daily calories from carbohydrates
+            $table->float('proteins_ratio') -> default(0.30); // Default 30% of daily calories from proteins
+            $table->float('fats_ratio') -> default(0.20); // Default 20% of daily calories from fats
+            $table->timestamps();
+        });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
-        //
+        Schema :: dropIfExists ('user_nutritional_settings');
     }
 };
