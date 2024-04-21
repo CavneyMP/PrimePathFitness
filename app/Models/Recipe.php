2bb5071351c982f643ed2f414ad08e7e8db0d1ad
CavneyMP01 <matthewcavney@yahoo.co.uk>
2024-04-21 21:37:54 +0100

    Mass assignable attributes for Recipes model

--- app/Models/Recipe.php
+++ app/Models/Recipe.php
@@ -3,9 +3,15 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Model; // Requirs Eloquent model
+
+// Recipe model class, extends Eloquent allowing interaction with databases.
 
 class Recipe extends Model
 {
     use HasFactory;
+
+    // Mass assigable attributes needed for meal planner.
+    protected $fillable = [ 'name', 'description', 'mealtype' ];  // $fillable property = ables mass assignment attributes from model
+
 }
