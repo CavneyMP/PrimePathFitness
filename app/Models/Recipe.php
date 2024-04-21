9aa6597cbfec3d304ac5cbf215f268a9906d332c
CavneyMP01 <matthewcavney@yahoo.co.uk>
2024-04-21 21:54:24 +0100

    Defined relationship for recipe model for ingredients table + form for activity level as will need it for macro adjustments

--- app/Models/Recipe.php
+++ app/Models/Recipe.php
@@ -14,4 +14,10 @@ class Recipe extends Model
     // Mass assigable attributes needed for meal planner.
     protected $fillable = [ 'name', 'description', 'mealtype' ];  // $fillable property = ables mass assignment attributes from model
 
+    // Defining the Relationship with RecipeIngredient table
+    public function ingredients() // Defines a method from Eloquent
+    {
+        return $this -> hasMany(RecipeIngredient::class); // hasMany method defines a one-to-many relationship. 1 Recipe has many RecipeIngredients
+    }
+    
 }
