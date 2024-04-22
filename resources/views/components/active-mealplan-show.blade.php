1c2941b2777ba8cdfee7d1cfa36a0916faa1493f
CavneyMP01 <matthewcavney@yahoo.co.uk>
2024-04-22 20:06:54 +0100

    UI element from tailwindUI package to work from

--- resources/views/components/active-mealplan-show.blade.php
+++ resources/views/components/active-mealplan-show.blade.php
@@ -1,19 +1,65 @@
-<div>
-        <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 px-4 pt-4">
-            <div class="mx-auto max-w-3xl">
-                <div class="bg-gray-800 p-6 rounded-lg shadow-lg">
-                    <h2 class="text-xl font-semibold leading-7 text-white"></h2>
-                    <p class="text-gray-300"></p>
+<div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 pt-4">
+    <div class="mx-auto max-w-3xl">
+        <div class="bg-gray-800 p-6 rounded-lg shadow-lg">
 
-                    <ul role="list" class="divide-y divide-gray-800">
-                            <li class="flex justify-between gap-x-6 py-5">
-                                <div class="flex min-w-0 gap-x-4">
-                                    <div class="min-w-0 flex-auto">
-                                        <p class="text-sm font-semibold leading-6 text-white"></p>
-                                        <p class="mt-1 truncate text-xs leading-5 text-gray-400"></p>
-                                    </div>
-                                </div>
-                            </li>
-                    </ul>
-                </div> <!-- end outer container -->
+        <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 pt-4">
+    <div class="mx-auto max-w-3xl">
+        <div class="bg-white dark:bg-gray-800 p-6 rounded-lg shadow-lg">
+
+            <div class="border-b border-gray-200 dark:border-gray-700 bg-white dark:bg-gray-800 px-4 py-5 sm:px-6">
+                <div class="-ml-4 -mt-2 flex flex-wrap items-center justify-between sm:flex-nowrap">
+                    <div class="ml-4 mt-2">
+                        <h3 class="text-base font-semibold leading-6 text-white">Job Postings</h3>
+                    </div>
+                    <div class="ml-4 mt-2 flex-shrink-0">
+                        <button type="button"
+                            class="relative inline-flex items-center rounded-md bg-indigo-600 dark:bg-indigo-700 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 dark:hover:bg-indigo-600 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Create
+                            new job</button>
+                    </div>
+                </div>
             </div>
+
+            <ul role="list"
+                class="divide-y divide-gray-100 dark:divide-gray-900 overflow-hidden bg-white dark:bg-gray-800 shadow-sm dark:ring-1 dark:ring-gray-900/5 sm:rounded-xl">
+                <li class="relative flex justify-between gap-x-6 px-4 py-5 hover:bg-gray-50 dark:hover:bg-gray-700 sm:px-6">
+                    <div class="flex min-w-0 gap-x-4">
+                        <img class="h-12 w-12 flex-none rounded-full bg-gray-50"
+                            src="https://images.unsplash.com/photo-1495121469083-5e074fc9b486?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTJ8fGNvbW1lbnRzfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&w=1000&q=80"
+                            alt="">
+                        <div class="min-w-0 flex-auto">
+                            <p class="text-sm font-semibold leading-6 text-white">
+                                <a href="#" class="text-white">
+                                    <span class="absolute inset-x-0 -top-px bottom-0"></span>
+                                    Leslie Alexander
+                                </a>
+                            </p>
+                            <p class="mt-1 flex text-xs leading-5 text-white">
+                                <a href="mailto:leslie.alexander@example.com"
+                                    class="relative truncate hover:underline text-white">leslie.alexander@example.com</a>
+                            </p>
+                        </div>
+                    </div>
+                    <div class="flex shrink-0 items-center gap-x-4">
+                        <div class="hidden sm:flex sm:flex-col sm:items-end">
+                            <p class="text-sm leading-6 text-white">Co-Founder / CEO</p>
+                            <p class="mt-1 text-xs leading-5 text-white">Last seen <time
+                                    datetime="2023-01-23T13:23Z">3h ago</time></p>
+                        </div>
+                        <svg class="h-5 w-5 flex-none text-white" viewBox="0 0 20 20" fill="currentColor"
+                            aria-hidden="true">
+                            <path fill-rule="evenodd"
+                                d="M7.21 14.77a.75.75 0 01.02-1.06L11.168 10 7.23 6.29a.75.75 0 111.04-1.08l4.5 4.25a.75.75 0 010 1.08l-4.5 4.25a.75.75 0 01-1.06-.02z"
+                                clip-rule="evenodd" />
+                        </svg>
+                    </div>
+                </li>
+            </ul>
+
+        </div> <!-- end inner container -->
+    </div> <!-- end middle container -->
+</div> <!-- end outer container -->
+
+
+        </div> <!-- end inner container -->
+    </div> <!-- end middle container -->
+</div> <!-- end outer container -->
