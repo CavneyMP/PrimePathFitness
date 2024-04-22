b63bd0aa3f8d233c95a36ca20242589dd212d3ef
CavneyMP01 <matthewcavney@yahoo.co.uk>
2024-04-22 01:31:48 +0100

    Relationships defined and fillables for mass assignables for userMealPlan

--- app/Models/UserMealPlan.php
+++ app/Models/UserMealPlan.php
@@ -8,4 +8,16 @@ use Illuminate\Database\Eloquent\Model;
 class UserMealPlan extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['user_id',  'start_date', 'end_date',  'active']; 
+
+    public function user() 
+    {
+        return $this -> belongsTo(User :: class);
+    }
+
+    public function recipes()
+    { 
+        return $this -> hasMany(MealPlanRecipe :: class);
+    }
 }
