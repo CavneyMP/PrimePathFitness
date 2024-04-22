48b4ea218b8d16bb5e1f1b4637f93062dfecc56d
CavneyMP01 <matthewcavney@yahoo.co.uk>
2024-04-22 01:29:43 +0100

    Relatioships and fillables for Recipe Ingredeints

--- app/Models/RecipeIngredient.php
+++ app/Models/RecipeIngredient.php
@@ -8,4 +8,16 @@ use Illuminate\Database\Eloquent\Model;
 class RecipeIngredient extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['recipe_id',  'ingredient_id', 'quantity']; 
+
+    public function recipe()
+    {
+        return $this->belongsTo(Recipe::class);
+    }
+
+    public function ingredient()
+    {
+        return $this->belongsTo(Ingredient::class);
+    }
 }
