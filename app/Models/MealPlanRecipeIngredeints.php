6077affcb7282758829242366cf726736f32a74d
CavneyMP01 <matthewcavney@yahoo.co.uk>
2024-04-22 01:35:40 +0100

    Same for MealPlanRecipeIngredient Model

--- app/Models/MealPlanRecipeIngredeints.php
+++ app/Models/MealPlanRecipeIngredeints.php
@@ -8,4 +8,16 @@ use Illuminate\Database\Eloquent\Model;
 class MealPlanRecipeIngredeints extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['meal_plan_recipe_id',  'ingredient_id',  'adjusted_quantity'];
+
+    public function mealPlanRecipe()
+    {
+        return $this -> belongsTo(MealPlanRecipe :: class);
+    }
+ 
+    public function ingredient()
+    {   
+        return $this -> belongsTo(Ingredient :: class);
+    }
 }
