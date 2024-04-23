93d7adfc0d9874585d6c96d1c6af3b3918d06dd8
CavneyMP01 <matthewcavney@yahoo.co.uk>
2024-04-23 22:28:57 +0100

    Created Models for data object

---
+++ app/Models/BodyMetricsData.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class BodyMetricsData extends Model
+{
+    use HasFactory;
+}
