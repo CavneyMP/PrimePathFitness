7f03821ce24a7d6ff562232fd982d5993968d679
CavneyMP01 <matthewcavney@yahoo.co.uk>
2024-04-23 22:39:27 +0100

    Model fillables for body metric data, and relation  to user for user ID

--- app/Models/BodyMetricsData.php
+++ app/Models/BodyMetricsData.php
@@ -8,4 +8,17 @@ use Illuminate\Database\Eloquent\Model;
 class BodyMetricsData extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'height_meter',
+        'weight_kilogram',
+        'max_heart_rate',
+    ];
+
+    // Relationship to User model
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
