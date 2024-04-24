1ebd4206e1b7226b5db709b65a5e39abe1f6a512
CavneyMP01 <matthewcavney@yahoo.co.uk>
2024-04-24 02:06:35 +0100

    Test for metrics store function, basic pass mock details and assert the data in the object, test passes as expected

--- tests/Feature/MealPlanCreateControllerTest.php
+++ tests/Feature/MealPlanCreateControllerTest.php
@@ -5,6 +5,8 @@ namespace Tests\Feature;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+
 
 class MealPlanCreateControllerTest extends TestCase
 {
--- tests/Feature/MetricsControllerTest.php
+++ tests/Feature/MetricsControllerTest.php
@@ -5,9 +5,48 @@ namespace Tests\Feature;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use App\Models\User;
+
+
 
 class MetricsControllerTest extends TestCase
 {
-    use RefreshDatabase; // Refresh data base between tests to avoid interference.
-    use WithoutMiddleware; // To disable any middleware important for controllers,
+    use RefreshDatabase;
+
+    public function testSuccessfulMetricsStorage()
+    {
+        // Create a user and ensure they are verified, was having issue without this
+        $user = User :: factory( )-> create([
+            'email_verified_at' => now(),
+        ]);
+
+        // Login simulation
+        $this -> actingAs($user);
+
+        // A POST request with necessary data
+        $response = $this    -> post(route('metrics.store'), [
+            'age'            => 25,
+            'weight'         => 70,
+            'height'         => 180,
+            'gender'         => 'Male',
+            'activity_level' => 'Moderately active',
+            'goal_weight'    => 'maintain'
+        ]);
+
+        // Check the correct response
+        $response   ->  assertRedirect(route('dashboard'));
+        $response   ->  assertSessionHas('success', 'Metrics updated successfully!');
+        $this       ->  assertDatabaseHas('user_metrics', [
+
+            'user_id'        => $user->id,
+            'age'            => 25,
+            'weight'         => 70,
+            'height'         => 180,
+            'gender'         => 'Male',
+            'activity_level' => 'Moderately active',
+            'goal_weight'    => 'maintain'
+
+        ]);
+    }
 }
