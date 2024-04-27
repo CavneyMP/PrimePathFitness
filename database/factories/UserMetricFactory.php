af472cd8ba84eb2615053b7313566187e883f61a
CavneyMP01 <matthewcavney@yahoo.co.uk>
2024-04-27 13:23:36 +0100

    Random BMI, BMR and TDEE for metrics controller.

--- database/factories/UserMetricFactory.php
+++ database/factories/UserMetricFactory.php
@@ -25,7 +25,15 @@ class UserMetricFactory extends Factory
             'height'             => $this -> faker -> numberBetween(150, 200), // height, raning 150 to 200cm
             'gender'             => $this -> faker -> randomElement(['Male', 'Female']), // Either gender
             'activity_level'     => $this -> faker -> randomElement(['Sedentary', 'Lightly active', 'Moderately active', 'Very active', 'Super active']), // activity level. 
-            'goal_weight'        => $this -> faker -> randomElement(['intensive_loss', 'moderate_loss', 'maintain', 'moderate_gain', 'extreme_gain']), // goal weight figure.
-        ];
+           'goal_weight'        => $this -> faker -> randomElement(['intensive_loss', 'moderate_loss', 'maintain', 'moderate_gain', 'extreme_gain']), // goal weight figure.
+            // Data calculated from the Metrics controller. 
+            'bmi'                => $this -> faker -> numberBetween(2, 40), // Underweight, health weight, over weight representation for the BMI 
+            'bmr'                => $this -> faker -> numberBetween(120, 2400),  // Represents an example basil metabolic rate between 1200 and 2400 kcal.
+            'tdee'               => $this -> faker -> numberBetween(1800, 3000), // Repreesnts total daily expenditure to kcal. 
+        
+        
+        ] ;
+
     }
+    
 }
